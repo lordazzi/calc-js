1b01c5c3958744cdafc7d14629a8a5a4b54cd96d
lordazzi <ricardoazzi91@hotmail.com>
2018-03-27 14:41:00 -0300

    Corrigindo problema na multiplicação

--- src/index.ts
+++ src/index.ts
@@ -53,24 +53,29 @@ export class Calc {
 
 
     public static multiply(n1: number, n2: number): number {
-        // if (n1 % 1 === 0 && n2 % 1 === 0) {
+        if (n1 % 1 === 0 && n2 % 1 === 0) {
             return n1 * n2;
-        // }
+        }
 
-        // let n1Length = String(n1).replace(/\d+\.?/, '').length,
-        //     n2Length = String(n2).replace(/\d+\.?/, '').length;
+        let n1Length: number, n2Length: number,
+            n1BaseDecimal: string | number = 1,
+            n2BaseDecimal: string | number = 1;
 
-        // let n1BaseDecimal: string | number = '1';
-        // while (length--) n1BaseDecimal = `${n1BaseDecimal}0`;
-        // n1BaseDecimal = Number(n1BaseDecimal);
+        if (n1 % 1 !== 0) {
+            n1Length = String(n1).replace(/\d+\.?/, '').length;
+            while (n1Length--) n1BaseDecimal = `${n1BaseDecimal}0`;
+            n1BaseDecimal = Number(n1BaseDecimal);
+        }
 
-        // let n2BaseDecimal: string | number = '1';
-        // while (length--) n2BaseDecimal = `${n2BaseDecimal}0`;
-        // n2BaseDecimal = Number(n2BaseDecimal);
+        if (n2 % 1 !== 0) {
+            n2Length = String(n2).replace(/\d+\.?/, '').length;
+            while (n2Length--) n2BaseDecimal = `${n2BaseDecimal}0`;
+            n2BaseDecimal = Number(n2BaseDecimal);
+        }
 
-        // n1 = Math.floor(n1 * n1BaseDecimal);
-        // n2 = Math.floor(n2 * n1BaseDecimal);
+        n1 = Math.floor(n1 * n1BaseDecimal);
+        n2 = Math.floor(n2 * n2BaseDecimal);
 
-        // return (n1 * n2) / (n1BaseDecimal * n1BaseDecimal);
+        return (n1 * n2) / (n1BaseDecimal * n2BaseDecimal);
     }
 }
