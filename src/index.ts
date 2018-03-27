0feed622c059090299ddf1a88c09a2eec7bf1151
lordazzi <ricardoazzi91@hotmail.com>
2018-03-27 14:33:11 -0300

    Removendo códig com problema

--- src/index.ts
+++ src/index.ts
@@ -53,24 +53,24 @@ export class Calc {
 
 
     public static multiply(n1: number, n2: number): number {
-        if (n1 % 1 === 0 && n2 % 1 === 0) {
+        // if (n1 % 1 === 0 && n2 % 1 === 0) {
             return n1 * n2;
-        }
+        // }
 
-        let n1Length = String(n1).replace(/\d+\.?/, '').length,
-            n2Length = String(n2).replace(/\d+\.?/, '').length;
+        // let n1Length = String(n1).replace(/\d+\.?/, '').length,
+        //     n2Length = String(n2).replace(/\d+\.?/, '').length;
 
-        let n1BaseDecimal: string | number = '1';
-        while (length--) n1BaseDecimal = `${n1BaseDecimal}0`;
-        n1BaseDecimal = Number(n1BaseDecimal);
+        // let n1BaseDecimal: string | number = '1';
+        // while (length--) n1BaseDecimal = `${n1BaseDecimal}0`;
+        // n1BaseDecimal = Number(n1BaseDecimal);
 
-        let n2BaseDecimal: string | number = '1';
-        while (length--) n2BaseDecimal = `${n2BaseDecimal}0`;
-        n2BaseDecimal = Number(n2BaseDecimal);
+        // let n2BaseDecimal: string | number = '1';
+        // while (length--) n2BaseDecimal = `${n2BaseDecimal}0`;
+        // n2BaseDecimal = Number(n2BaseDecimal);
 
-        n1 = Math.floor(n1 * n1BaseDecimal);
-        n2 = Math.floor(n2 * n1BaseDecimal);
+        // n1 = Math.floor(n1 * n1BaseDecimal);
+        // n2 = Math.floor(n2 * n1BaseDecimal);
 
-        return (n1 * n2) / (n1BaseDecimal * n1BaseDecimal);
+        // return (n1 * n2) / (n1BaseDecimal * n1BaseDecimal);
     }
 }
