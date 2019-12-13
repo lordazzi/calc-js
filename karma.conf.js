bdf57638e095b4c2ef30c7c561d365cfa1f3ff91
RICARDO AZZI SILVA <ricardo.azzi-ext@viavarejo.com.br>
2019-12-13 13:09:45 -0300

    Atualização de bibliotecas de dependência

--- karma.conf.js
+++ karma.conf.js
@@ -1,75 +1,67 @@
-module.exports = function(config) {
-    config.set({
+module.exports = function (config) {
+  config.set({
 
-        basePath: '',
+    basePath: '',
 
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-typescript-preprocessor'),
-            require('karma-systemjs')
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-typescript')
+    ],
+
+    frameworks: [
+      'karma-typescript',
+      'jasmine'
+    ],
+
+    files: [
+      'src/*.ts'
+    ],
+
+    preprocessors: {
+      "src/**/*.ts": "karma-typescript"
+    },
+
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222',
         ],
+      }
+    },
 
-        frameworks: [
-            'systemjs',
-            'jasmine'
-        ],
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
-        files: [
-            'src/*.spec.ts'
-        ],
+    // web server port
+    port: 9876,
 
-        preprocessors: {
-            '*.ts': ['typescript']
-        },
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-        systemjs: {
-            configFile: 'systemjs.config.js',
-            serveFiles: [
-                'src/**/*.ts'
-            ]
-        },
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-        coverageIstanbulReporter: {
-            reports: ['html', 'lcovonly'],
-            fixWebpackSourcePaths: true
-        },
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-        customLaunchers: {
-            ChromeHeadless: {
-                base: 'Chrome',
-                flags: [
-                    '--headless',
-                    '--disable-gpu',
-                    '--remote-debugging-port=9222',
-                ],
-            }
-        },
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: [ 'progress' ],
-
-        // web server port
-        port: 9876,
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['ChromeHeadless'],
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true
-    });
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
 };
