97757b9a04722f127b9b242eadf652207401af26
RICARDO AZZI SILVA <ricardo.azzi-ext@viavarejo.com.br>
2020-04-24 17:49:57 -0300

    Incluindo no index.ts uma forma de exportação para node e para browser

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import { Calc } from './calc';
 export * from './calc';
 export * from './config/calc-config';
 export * from './error/calc-error';
 
+declare const window: Window & { [prop: string]: any };
+
+if (window) {
+  window.Calc = Calc;
+}
+
+if ('exports' in module) {
+  module.exports = Calc;
+}
