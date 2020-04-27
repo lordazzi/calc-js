a0a7c47a0cbcf4ee3b275d0c49a6f32b05ba6757
RICARDO AZZI SILVA <ricardo.azzi-ext@viavarejo.com.br>
2020-04-27 09:49:17 -0300

    Criando js head para exportar a lib para javascript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { Calc } from './calc';
 export * from './calc';
 export * from './config/calc-config';
 export * from './error/calc-error';
 
-declare const window: Window & { [prop: string]: any };
-
-if (window) {
-  window.Calc = Calc;
-}
-
-if ('exports' in module) {
-  module.exports = Calc;
-}
