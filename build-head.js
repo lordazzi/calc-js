6dd1ad53df48aa4ec0fda6fd775cd0d3e28ba3ff
Ricardo Azzi Silva <ricardoazzi91@hotmail.com>
2024-03-31 15:18:51 -0300

    removendo build javascript

--- build-head.js
+++
@@ -1,4 +0,0 @@
-var define = typeof define !== 'undefined' ? define : function define(name, argsNames, calle) {
-  define.clazzes = define.clazzes || {}
-  calle.apply(null, argsNames.map(() => define.clazzes));
-};
